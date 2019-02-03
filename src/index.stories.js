5c7a1020deac3f7eab1047fc46f30332fd841ab8
Daniel O'Connor <doconnor@thumbtack.com>
2019-02-03 12:22:32 -0800

    Update Storybook with new API

--- src/index.stories.js
+++ src/index.stories.js
@@ -77,59 +77,66 @@ storiesOf("Overview", module).add("full example", () => (
         </p>
 
         <ScrollMarkerSection id="history">
-          <h2>History</h2>
+          {({ id }) => (
+            <section>
+              <h2 id={id}>History</h2>
 
-          <p>
-            Nullam facilisis justo nec felis interdum, sed iaculis purus porta.
-            Pellentesque nec tellus condimentum, varius ipsum ac, vulputate
-            urna. Aliquam a bibendum sapien. Cras varius eu turpis id ultricies.
-            Morbi porttitor erat libero, in malesuada purus tristique vel.
-            Maecenas sapien nisl, condimentum at sodales a, efficitur feugiat
-            enim. Aliquam sit amet nibh ligula. Ut tortor nunc, molestie et arcu
-            vitae, aliquam mattis neque. Nam iaculis, lacus eu commodo rutrum,
-            nisi dui tempor nisi, non fringilla augue lectus eu augue. Maecenas
-            ac scelerisque elit. Donec mollis ex scelerisque pellentesque
-            lobortis. Phasellus id viverra sapien. Integer ultricies, urna non
-            blandit consectetur, ex erat lobortis dui, ut sollicitudin diam
-            justo sed metus. Nunc vel nunc cursus, euismod ex eget, ullamcorper
-            turpis. Fusce at dignissim ligula.
-          </p>
+              <p>
+                Nullam facilisis justo nec felis interdum, sed iaculis purus
+                porta. Pellentesque nec tellus condimentum, varius ipsum ac,
+                vulputate urna. Aliquam a bibendum sapien. Cras varius eu turpis
+                id ultricies. Morbi porttitor erat libero, in malesuada purus
+                tristique vel. Maecenas sapien nisl, condimentum at sodales a,
+                efficitur feugiat enim. Aliquam sit amet nibh ligula. Ut tortor
+                nunc, molestie et arcu vitae, aliquam mattis neque. Nam iaculis,
+                lacus eu commodo rutrum, nisi dui tempor nisi, non fringilla
+                augue lectus eu augue. Maecenas ac scelerisque elit. Donec
+                mollis ex scelerisque pellentesque lobortis. Phasellus id
+                viverra sapien. Integer ultricies, urna non blandit consectetur,
+                ex erat lobortis dui, ut sollicitudin diam justo sed metus. Nunc
+                vel nunc cursus, euismod ex eget, ullamcorper turpis. Fusce at
+                dignissim ligula.
+              </p>
 
-          <h3>Establishment</h3>
+              <h3>Establishment</h3>
 
-          <p>
-            Nullam facilisis justo nec felis interdum, sed iaculis purus porta.
-            Pellentesque nec tellus condimentum, varius ipsum ac, vulputate
-            urna. Aliquam a bibendum sapien. Cras varius eu turpis id ultricies.
-            Morbi porttitor erat libero, in malesuada purus tristique vel.
-            Maecenas sapien nisl, condimentum at sodales a, efficitur feugiat
-            enim. Aliquam sit amet nibh ligula. Ut tortor nunc, molestie et arcu
-            vitae, aliquam mattis neque. Nam iaculis, lacus eu commodo rutrum,
-            nisi dui tempor nisi, non fringilla augue lectus eu augue. Maecenas
-            ac scelerisque elit. Donec mollis ex scelerisque pellentesque
-            lobortis. Phasellus id viverra sapien. Integer ultricies, urna non
-            blandit consectetur, ex erat lobortis dui, ut sollicitudin diam
-            justo sed metus. Nunc vel nunc cursus, euismod ex eget, ullamcorper
-            turpis. Fusce at dignissim ligula.
-          </p>
+              <p>
+                Nullam facilisis justo nec felis interdum, sed iaculis purus
+                porta. Pellentesque nec tellus condimentum, varius ipsum ac,
+                vulputate urna. Aliquam a bibendum sapien. Cras varius eu turpis
+                id ultricies. Morbi porttitor erat libero, in malesuada purus
+                tristique vel. Maecenas sapien nisl, condimentum at sodales a,
+                efficitur feugiat enim. Aliquam sit amet nibh ligula. Ut tortor
+                nunc, molestie et arcu vitae, aliquam mattis neque. Nam iaculis,
+                lacus eu commodo rutrum, nisi dui tempor nisi, non fringilla
+                augue lectus eu augue. Maecenas ac scelerisque elit. Donec
+                mollis ex scelerisque pellentesque lobortis. Phasellus id
+                viverra sapien. Integer ultricies, urna non blandit consectetur,
+                ex erat lobortis dui, ut sollicitudin diam justo sed metus. Nunc
+                vel nunc cursus, euismod ex eget, ullamcorper turpis. Fusce at
+                dignissim ligula.
+              </p>
 
-          <h3>Presidents</h3>
+              <h3>Presidents</h3>
 
-          <p>
-            Nullam facilisis justo nec felis interdum, sed iaculis purus porta.
-            Pellentesque nec tellus condimentum, varius ipsum ac, vulputate
-            urna. Aliquam a bibendum sapien. Cras varius eu turpis id ultricies.
-            Morbi porttitor erat libero, in malesuada purus tristique vel.
-            Maecenas sapien nisl, condimentum at sodales a, efficitur feugiat
-            enim. Aliquam sit amet nibh ligula. Ut tortor nunc, molestie et arcu
-            vitae, aliquam mattis neque. Nam iaculis, lacus eu commodo rutrum,
-            nisi dui tempor nisi, non fringilla augue lectus eu augue. Maecenas
-            ac scelerisque elit. Donec mollis ex scelerisque pellentesque
-            lobortis. Phasellus id viverra sapien. Integer ultricies, urna non
-            blandit consectetur, ex erat lobortis dui, ut sollicitudin diam
-            justo sed metus. Nunc vel nunc cursus, euismod ex eget, ullamcorper
-            turpis. Fusce at dignissim ligula.
-          </p>
+              <p>
+                Nullam facilisis justo nec felis interdum, sed iaculis purus
+                porta. Pellentesque nec tellus condimentum, varius ipsum ac,
+                vulputate urna. Aliquam a bibendum sapien. Cras varius eu turpis
+                id ultricies. Morbi porttitor erat libero, in malesuada purus
+                tristique vel. Maecenas sapien nisl, condimentum at sodales a,
+                efficitur feugiat enim. Aliquam sit amet nibh ligula. Ut tortor
+                nunc, molestie et arcu vitae, aliquam mattis neque. Nam iaculis,
+                lacus eu commodo rutrum, nisi dui tempor nisi, non fringilla
+                augue lectus eu augue. Maecenas ac scelerisque elit. Donec
+                mollis ex scelerisque pellentesque lobortis. Phasellus id
+                viverra sapien. Integer ultricies, urna non blandit consectetur,
+                ex erat lobortis dui, ut sollicitudin diam justo sed metus. Nunc
+                vel nunc cursus, euismod ex eget, ullamcorper turpis. Fusce at
+                dignissim ligula.
+              </p>
+            </section>
+          )}
         </ScrollMarkerSection>
 
         <ScrollMarkerSection id="organization">
